76b6e77914ab6215b27bb893e3aee3b4204fddb1
Marcin Jasion <marcinjasion@gmail.com>
2015-11-11 20:30:52 +0100

    fix files

--- dstat/addColumns.groovy
+++ dstat/addColumns.groovy
@@ -1,40 +1,49 @@
+import groovy.transform.Field
+
 // its not CLEAN, just CODE
 File directory = new File('.')
 
 List<File> files = directory.listFiles().collect().findAll { File f -> f.isFile() }.sort(false) { File file -> file.name }
+//List<File> files = [new File('./tomcat-clean-100th-mongo.csv')]
 files.remove(new File('./addColumns.groovy'))
-println files
-
+files.remove(new File('./import_dstat.sql'))
+println files.join('\n')
+println ''
 List<String> testGroups = ['API Validation', 'Key Validation', 'CRUD', 'ALL']
-
+File destinationDirectory = new File('./new')
+destinationDirectory.listFiles()*.delete()
 files.each { File oldCsv ->
 
     String fileName = oldCsv.name.replace('.csv', '')
+    println "fileName = $fileName"
     String lang = fileName.split('-')[0].toUpperCase()
     String test_type = fileName.split('-')[1].toUpperCase()
     String test_threads = fileName.split('-')[2]
     String server = fileName.split('-')[3]
 
     int testGroupIndex = 0
-    File newCsv = new File(new File('./new'), "$fileName-new.csv")
+    File newCsv = new File(destinationDirectory, "$fileName-new.csv")
     newCsv.text = ''
     println "lang = $lang"
     println "test_type = $test_type"
     println "test_threads = $test_threads"
     println "server = $server"
 
-    oldCsv.eachWithIndex { String line, int i ->
+    String firstDstatDate = oldCsv.readLines().first().split(',').first()
+    long firstDate = Date.parse('yyy-dd-MM HH:mm:ss', "2015-$firstDstatDate").time / 1000
+
+    oldCsv.eachWithIndex { String line, int index ->
         if (line.size() < 5) { // a bo tak
             testGroupIndex += 1
-            println "i = $i\ttestGroupIndex = $testGroupIndex"
+            println "$index: ${testGroups[testGroupIndex]}"
         } else {
             String testGroup = testGroups[testGroupIndex]
-
             def dstatDate = line.split(',').first()
             Date lineDate = Date.parse('yyy-dd-MM HH:mm:ss', "2015-$dstatDate")
-            long seconds = lineDate.time / 1000
+            long seconds = (lineDate.time / 1000) - firstDate
             newCsv.append(line.replaceFirst(dstatDate, seconds.toString()) + ",$lang,$test_type,$test_threads,$server,$testGroup\n")
         }
     }
+    assert testGroupIndex == 3
     println ''
 }
--- dstat/import_dstat.sql
+++ dstat/import_dstat.sql
@@ -1,6 +1,6 @@
-drop table if EXISTS  dstat_tmp CASCADE ;
+DROP TABLE IF EXISTS dstat_tmp CASCADE;
+DROP TABLE IF EXISTS dstat_time_diff CASCADE;
 CREATE TABLE dstat_tmp (
-  id                 BIGSERIAL NOT NULL,
   seconds_from_start BIGINT    NOT NULL,
   cpu_usr            FLOAT     NOT NULL,
   cpu_sys            FLOAT     NOT NULL,
@@ -27,17 +27,63 @@ CREATE TABLE dstat_tmp (
   test_group         TEXT      NOT NULL
 );
 
-
 COPY dstat_tmp (seconds_from_start, cpu_usr, cpu_sys, cpu_idl, cpu_wai, cpu_hiq, cpu_siq, disk_read, disk_write, net_recv, net_send, paging_in, paging_out, system_int, system_csw, memory_used, memory_buff, memory_cache, memory_free, test_language, test_type, test_threads, test_server, test_group)
 FROM '/csv/ALL.csv' DELIMITER ',' CSV;
 
+DROP TABLE IF EXISTS dstat_time_diff CASCADE;
 CREATE TABLE dstat_time_diff AS
   SELECT
     test_language,
     test_type,
     test_group,
     test_server,
-    min(seconds_from_start) min_sec,
-  FROM jmeter_result_tmp
+    min(seconds_from_start) min_sec
+  FROM dstat_tmp
   GROUP BY test_language, test_type, test_group, test_server;
+
+DROP TABLE dstat CASCADE;
+CREATE TABLE dstat AS
+  SELECT
+    (seconds_from_start - (SELECT td.min_sec
+                           FROM dstat_time_diff td
+                           WHERE d.test_language = td.test_language AND d.test_type = td.test_type AND
+                                 d.test_group = td.test_group AND td.test_server = d.test_server)) seconds_from_start,
+
+    cpu_usr,
+    cpu_sys,
+    cpu_idl,
+    cpu_wai,
+    cpu_hiq,
+    cpu_siq,
+    disk_read,
+    disk_write,
+    net_recv,
+    net_send,
+    paging_in,
+    paging_out,
+    system_int,
+    system_csw,
+    memory_used,
+    memory_buff,
+    memory_cache,
+    memory_free,
+    test_language,
+    test_type,
+    substring(test_threads from 1 for 3)::INT as test_threads,
+    upper(test_server)                                                                             test_server,
+    test_group :: test_group
+  FROM dstat_tmp d;
+
+SELECT
+  test_language,
+  test_type,
+  test_group,
+  test_server,
+  test_threads,
+  min(seconds_from_start),
+  max(seconds_from_start),
+  count(*)
+FROM dstat
+GROUP BY test_language, test_group, test_type, test_threads, test_server
+ORDER BY test_language, test_type, test_group, test_threads, test_server;
 
